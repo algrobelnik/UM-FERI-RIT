df1edc69d98bd7a7b09ace21854a39309595cfc3
Aleksander <aleksander.grobelnik1@gmail.com>
2021-06-10 11:22:07 +0000

    [*]APS code visual optimization

--- ARA/vaja4/vaja4.cpp
+++ ARA/vaja4/vaja4.cpp
@@ -22,12 +22,12 @@ int sum(vector<int> vec, int min, int max) {
 
 int main(int argc, char **argv) {
   if (argc != 4) {
-   cout << "Error: arguments are not OK" << endl;
-   return -1;
+    cout << "Error: arguments are not OK" << endl;
+    return -1;
   }
   if (argv[1][0] != 'c' && argv[1][0] != 'd') {
-   cout << "argv[1] is not c or d" << endl;
-   return -2;
+    cout << "argv[1] is not c or d" << endl;
+    return -2;
   }
   int len = atoi(argv[2]);
   const int b_len = ceil(log2(len + 1));
@@ -49,30 +49,25 @@ int main(int argc, char **argv) {
       } else {
         auto it = find(S.begin(), S.end(), T);
         int tmp = distance(S.begin(), it);
-        for (int i = b_len - 1; i >= 0; i--) {
+        for (int i = b_len - 1; i >= 0; i--)
           bw.writeBit((tmp >> i) & 1);
-        }
-        // out.push_back(hldr);
         S.push_back(T + val);
         if (S.size() >= len) {
           S.clear();
-          for (int i = 0; i < 256; i++) {
+          for (int i = 0; i < 256; i++)
             S.push_back(std::string(1, i));
-          }
         }
         T = val;
       }
     }
     auto it = find(S.begin(), S.end(), T);
     int tmp = distance(S.begin(), it);
-    for (int i = b_len - 1; i >= 0; i--) {
+    for (int i = b_len - 1; i >= 0; i--)
       bw.writeBit((tmp >> i) & 1);
-    }
     cout << b_sum * 8 << endl;
     cout << bw.b << endl;
     float ratio = (b_sum * 8 / (float)bw.b);
     cout << "Ratio: " << ratio << endl;
-    //bw.writeByte(bw.x);
   } else {
     BinReader br(argv[3]);
     BinWriter bw("out_d.bin");
@@ -81,29 +76,25 @@ int main(int argc, char **argv) {
     br.readByte();
     while (br.f.peek() != EOF) {
       string b = "";
-      for (int i = 0; i < b_len; i++) {
+      for (int i = 0; i < b_len; i++)
         b += to_string(br.readBit());
-      }
       int ind = stoi(b, 0, 2);
       tmp = "";
       if (S.size() <= ind) {
         tmp = T + T[0];
         S.push_back(tmp);
-      }else{
+      } else
         tmp = S[ind];
-      }
-      for (int i = 0; i < tmp.length(); i++) {
+      for (int i = 0; i < tmp.length(); i++)
         bw.writeByte(tmp[i]);
-      }
       if (!tmp.empty()) {
         char val = tmp[0];
         if (find(S.begin(), S.end(), T + val) == S.end()) {
           S.push_back(T + val);
           if (S.size() >= len) {
             S.clear();
-            for (int i = 0; i < 256; i++) {
+            for (int i = 0; i < 256; i++)
               S.push_back(std::string(1, i));
-            }
           }
         }
       }
