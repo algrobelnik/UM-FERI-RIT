b44d587483d6e2bd2df759e15132e3f154fc9daa
profek10 <aleksander.grobelnik1@gmail.com>
2021-11-05 09:37:34 +0100

    [*]vaja01: minor code optimization

--- OS/vaja01/project.sh
+++ OS/vaja01/project.sh
@@ -13,32 +13,13 @@ Options:
 function echoWrongUsage {
   echo "WRONG USAGE: try project --help"
 }
-INDEX=0
-DIR="$(dirname "${BASH_SOURCE}")"
-for i in "$@"
-do
-  args[$INDEX]=$i
-  let INDEX=${INDEX}+1
-done
-if [ ${args[0]} = "new" ] || [ ${args[0]} = "add" ] || [ ${args[0]} = "test" ] || [ "${args[0]}" = "-h" -o "${args[0]}" = "-?" -o "${args[0]}" = "--help" ];  then
-  if [ "${args[0]}" = "-h" -o "${args[0]}" = "-?" -o "${args[0]}" = "--help" ];  then
-    helpInfo
-    exit 1
-  fi
-  a=2
-  params=${args[1]}
-  while [[ $a -lt ${#args}+1 ]]
-  do
-    params=${params}" ${args[$a]}"
-    let a=$a+1
-  done
-  bash project_${args[0]}.sh $params
-  if [ "$?" -eq 0 ];  then
-    echo -e "sucess"
-  else
-    echo -e "fail"
-    let FCOUNT="${FCOUNT}"+1
-  fi
+args=($@)
+if [[ "${1}" = "-h" ]] || [[ "${1}" = "-?" ]] || [[ "${1}" = "--help" ]];  then
+  helpInfo
+  exit 1
+fi
+if [[ ${1} = "new" ]] || [[ ${1} = "add" ]] || [[ ${1} = "test" ]];  then
+  bash project_${1}.sh ${args[@]:1}
 else
   echoWrongUsage
   exit 1
