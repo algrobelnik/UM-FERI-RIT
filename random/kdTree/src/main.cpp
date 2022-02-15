e7cec8c9f3f4f5355e9a53dcadde3a3e93d94d38
profek10 <aleksander.grobelnik1@gmail.com>
2022-02-15 22:33:20 +0100

    [*]kdTree: code rewrite + ready for algorithm

--- random/kdTree/src/main.cpp
+++ random/kdTree/src/main.cpp
@@ -2,25 +2,61 @@
 #include <iostream>
 
 using namespace std;
+//const int len1 = 1000000;
+const int len1 = 100;
+const int len2 = 2;
 
-double distance(double *a, double *b) {
-  return sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2));
+double distance(double *arr, double *val, int offset) {
+  //cout << "OFFSET: " << offset << " ";
+  double sum = 0;
+  for (int i = 0; i < len2; i++) {
+    //cout << arr[i + offset] << " ";
+    sum += pow(arr[i + offset] - val[i + offset], 2);
+  }
+  //cout << "SUM: " << sum << " | " << sqrt(sum) << endl;
+  return sqrt(sum);
+}
+
+double randGen(int min, int max) {
+  int range = max - min++;
+  return rand() % range + min;
+}
+
+void fillArr(double *arr) {
+  for (int i = 0; i < len1; i++) {
+    for (int j = 0; j < len2; j++) {
+      arr[i * len2 + j] = randGen(-10, 10);
+    }
+  }
 }
 
 int main() {
-  double val[2];
-  cout << "coordinates: ";
-  cin >> val[0] >> val[1];
-  double arr[7][2] = {{1, 2}, {3, 2}, {4, 6}, {6, 4}, {7, 8}, {9, 9}, {5.4, 8.4},};
-  double *res = arr[0];
-  double dist = distance(res, val);
-  for (int i = 1; i < 6; i++) {
-    if (distance(res, val) > distance(arr[i], val)) {
-      res = arr[i];
-      dist = distance(arr[i], val);
+  srand(time(nullptr));
+  double val[len2];
+  cout << "coordinates(" << len2 << "): ";
+  for (int i = 0; i < len2; i++) {
+    cin >> val[i];
+  }
+  double *arr = new double[len1 * len2];
+  fillArr(arr);
+  int offset = 0;
+  double dist = distance(arr, val, 0);
+  for (int i = len2; i < len1; i += len2) {
+    double tmp = distance(arr, val, i);
+    if (dist > tmp) {
+      offset = i;
+      dist = tmp;
     }
   }
-  cout << "Closes element to (" << val[0] << "," << val[1] << ") is (" << res[0]
-       << "," << res[1] << ")[" << dist << "]" << endl;
+  cout << "Closes element to (";
+  for (int i = 0; i < len2; i++) {
+    cout << (i != 0 ? "," : "") << val[i];
+  }
+  cout << ") is (";
+  for (int i = 0; i < len2; i++) {
+    cout << (i != 0 ? "," : "") << arr[offset + i];
+  }
+  cout << ")[" << dist << "]" << endl;
+  delete[] arr;
   return 0;
 }
